0cf90288b0dfdfc7e3817e9e06a2191f234bf758
Draconis187 <christopherkyledejager@gmail.com>
2026-07-10 10:33:18 +0100

    Created tables and models for the project

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+from django.conf import settings
+
 
 # Create your models here.
+class Courses(models.Model):
+    CourseID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    CreatorID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    Name = models.CharField(max_length=100)
+    Description = models.CharField(max_length=1000)
+    Subject = models.CharField(max_length=50)
+    NumberOfSteps = models.SmallIntegerField()
+    CreatedDate = models.DateField(auto_now=False, auto_now_add=True)
+    IsDeleted = models.BooleanField()
+
+class StudentsAndCourses(models.Model):
+    SACID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    UserID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    CourseID = models.ForeignKey(Courses, on_delete=models.PROTECT)
+    CurrentProgress = models.SmallIntegerField()
+    Completed = models.BooleanField()
+    CompletedDate = models.DateField(auto_now=True, auto_now_add=False)

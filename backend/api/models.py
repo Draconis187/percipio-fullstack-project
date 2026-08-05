ff858bf37d19e78585ec76897d20cf7c40e01783
Draconis187 <christopherkyledejager@gmail.com>
2026-08-05 14:40:31 +0100

    Added a user manager for the extended user model, created the new user model and updated existing models to use the unique identifier as the foreign key to link them.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 import uuid
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 
 # Create your models here.
+
+class LMSUserManager(BaseUserManager):
+    def create_user(self, username, password=None, groupType=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is a required field")
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("userType", 0)
+        return self.create_user(username, password, **extra_fields)
+
+
+class LMSUser(AbstractUser):
+    userType = models.SmallIntegerField(null=True)
+    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50, unique=True)
+
+    objects = LMSUserManager()
+
 class Courses(models.Model):
     CourseID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    CreatorID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    CreatorID = models.ForeignKey(LMSUser, on_delete=models.PROTECT)
     Name = models.CharField(max_length=100)
     Description = models.CharField(max_length=1000)
     Subject = models.CharField(max_length=50)
@@ -19,8 +45,10 @@ class Courses(models.Model):
 
 class StudentsAndCourses(models.Model):
     SACID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    UserID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    UserID = models.ForeignKey(LMSUser, on_delete=models.PROTECT)
     CourseID = models.ForeignKey(Courses, on_delete=models.PROTECT)
     CurrentProgress = models.SmallIntegerField()
     Completed = models.BooleanField()
     CompletedDate = models.DateField(auto_now=False, auto_now_add=False)
+
+

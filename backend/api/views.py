8108fc62451ecc498d25c20b8f0baade50fda9e7
Draconis187 <christopherkyledejager@gmail.com>
2026-08-05 14:44:46 +0100

    Added the user and login viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,15 @@ from rest_framework import viewsets, permissions
 from .serializers import *
 from rest_framework.response import Response
 from .models import *
+from django.contrib.auth import login, authenticate
+from django.shortcuts import get_object_or_404
+from knox.models import AuthToken
+
+# To ensure the latest user model is always fetched (to test later if current implementation does not update)
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+
+
 
 # Create your views here.
 def home(request):
@@ -11,6 +20,7 @@ def home(request):
 
 class CourseViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
+    # permission_classes = [permissions.IsAuthenticated]
     queryset = Courses.objects.all()
     serializer_class = CourseSerializer
 
@@ -70,5 +80,66 @@ class StudentAndCoursesViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+class UsersViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = LMSUser.objects.all()
+    serializer_class = UsersSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors, status=400)
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UsersSerializer(user)
+        return Response(serializer.data)
+
+
+class LoginViewset(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data["username"]
+            password = serializer.validated_data["password"]
+
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                _, token=AuthToken.objects.create(user)
+                return Response(
+                    
+                    {
+                        "id":user.pk,
+                        "user":self.serializer_class(user).data,
+                        "token": token
+                    }
+                        
+                )
+            else: 
+                return Response(
+                    {
+                        "error": "Invalid username or password"
+                    }, 
+                    status=401
+                    )
+
         else:
             return Response(serializer.errors, status=400)

ecfc394e36f3d78dd82dc9af914cbeab04e91dd2
Draconis187 <christopherkyledejager@gmail.com>
2026-08-05 14:42:53 +0100

    Addes the users and login viewsets. Deleted the commented url patterns section as it is not needed.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,8 +5,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("courses",CourseViewSet, basename="courses")
 router.register("studentsAndCourses",StudentAndCoursesViewSet, basename="studentsAndCourses")
+router.register("users",UsersViewSet, basename="users")
+router.register("login",LoginViewset, basename="login")
 urlpatterns = router.urls
-
-# urlpatterns = [
-#     path('', home)
-# ]

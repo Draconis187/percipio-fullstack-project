01cfbede435fd931db54a1d4ef82c6b903434053
Draconis187 <christopherkyledejager@gmail.com>
2026-08-05 14:41:56 +0100

    Added the user serializer to create new users and created the login serializer.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth import get_user_model
+import uuid
+
+User = get_user_model()
+
+# To ensure the latest user model is always fetched (to test later if current implementation does not update)
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,15 +21,7 @@ class CourseSerializer(serializers.ModelSerializer):
                     "NumberOfSteps",
                     "IsDeleted"
                 )
-
-class CourseDeleteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Courses
-        fields = (
-                    "CourseID",
-                    "IsDeleted"
-                )
-        
+       
 
 class StudentAndCoursesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -33,3 +33,30 @@ class StudentAndCoursesSerializer(serializers.ModelSerializer):
                     "Completed",
                     "CompletedDate"
                 )        
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    # This field is only for writing (when creating a user), not for reading.
+    # We require a minimum password length.
+    # password = serializers.CharField(write_only=True, min_length=8) 
+
+    class Meta:
+        model = User
+        fields = ("username","email","password", "userType")
+        read_only_fields = ("id",)
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, valid_data):
+        user = User.objects.create_user(**valid_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret.pop('password', None)
+        return ret

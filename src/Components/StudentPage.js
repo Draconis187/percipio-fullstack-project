d65e874860d74f23f687e1aac304fd5bf96e4a41
Draconis187 <christopherkyledejager@gmail.com>
2026-09-08 11:09:12 +0100

    Created student main page that they navigate to.

---
+++ src/Components/StudentPage.js
@@ -0,0 +1,33 @@
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import NavBar from "./NavBar";
+import StudentEnroll from "./StudentEnroll";
+
+const StudentPage = () => {
+  const navigate = useNavigate();
+
+  const checkUserCredentials = () => {
+    const userType = sessionStorage.getItem("userType");
+    if (userType == 2) {
+      navigate(`/homePage`);
+    } else if (userType == 0) {
+      navigate(`/adminPage`);
+    }
+  };
+
+  useEffect(() => {
+    checkUserCredentials();
+  }, []);
+
+  return (
+    <Box>
+      <div>
+        <NavBar />
+        <StudentEnroll />
+      </div>
+    </Box>
+  );
+};
+
+export default StudentPage;

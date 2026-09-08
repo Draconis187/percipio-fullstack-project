ecab1afd72a32a5707f274d459bc4320988811a9
Draconis187 <christopherkyledejager@gmail.com>
2026-09-08 10:48:32 +0100

    Corrected a route and added new routes. Removed unneeded code.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
-import StudentEnroll from "./Components/StudentEnroll";
 import AddCourse from "./Components/AddCourse";
 import HomePage from "./Components/HomePage";
 import EditCourse from "./Components/EditCourse";
@@ -10,36 +8,36 @@ import CourseDetails from "./Components/CourseDetails";
 import AdminPage from "./Components/AdminPage";
 import EditUser from "./Components/EditUser";
 import AddUser from "./Components/AddUser";
+import StudentPage from "./Components/StudentPage";
+import ProtectedRoute from "./Components/ProtectedRoutes";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <Box className="App">
+      <CssBaseline />
       <Routes>
-        <Route path="" element={<Login />} />
-        <Route path="/studentEnroll" element={<StudentEnroll />} />
-        <Route path="homePage/addCourse" element={<AddCourse />} />
-        <Route path="/homePage" element={<HomePage />} />
-        <Route path="homePage/editCourse/:id" element={<EditCourse />} />
-        <Route path="homePage/courseDetails/:id" element={<CourseDetails />} />
-        <Route path="/adminPage" element={<AdminPage />} />
-        <Route path="adminPage/addUser" element={<AddUser />} />
-        <Route path="admin/editUser" element={<EditUser />} />
+        <Route path="/" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="homePage/addCourse" element={<AddCourse />} />
+          <Route path="/homePage" element={<HomePage />} />
+          <Route path="homePage/editCourse/:id" element={<EditCourse />} />
+          <Route
+            path="homePage/courseDetails/:id"
+            element={<CourseDetails />}
+          />
+          <Route path="/studentPage" element={<StudentPage />} />
+          <Route
+            path="studentPage/courseDetails/:id"
+            element={<CourseDetails />}
+          />
+          <Route path="/adminPage" element={<AdminPage />} />
+          <Route path="adminPage/addUser" element={<AddUser />} />
+          <Route path="adminPage/editUser/:id" element={<EditUser />} />
+        </Route>
       </Routes>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    </Box>
   );
 }
 

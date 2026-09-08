529a50aaa1463bf396c5b68a2a053858e6fe2229
Draconis187 <christopherkyledejager@gmail.com>
2026-09-08 10:53:46 +0100

    Added interceptors for authorised users and to logout the user.

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//Get environment variable to change baseurl
+
 const baseUrl = "http://127.0.0.1:8000/";
 const AxiosInstance = axios.create({
   baseURL: baseUrl,
@@ -9,5 +11,28 @@ const AxiosInstance = axios.create({
     accept: "application/json",
   },
 });
+
+AxiosInstance.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("Token");
+
+  if (token) {
+    config.headers.Authorization = `Token ${token}`;
+  } else {
+    config.headers.Authorization = ``;
+  }
+  return config;
+});
+
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      sessionStorage.removeItem("Token");
+      sessionStorage.removeItem("userId");
+    }
+  },
+);
 
 export default AxiosInstance;

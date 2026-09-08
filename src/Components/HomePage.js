3d3cb37f4231174c3de892c3be0a0a1f0ffc8f83
Draconis187 <christopherkyledejager@gmail.com>
2026-09-08 11:03:13 +0100

    Added in the navbar, CSS baseline and user type check to redirect students to the correct page.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,12 +1,34 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import AddCourse from "./AddCourse";
 import CourseList from "./CourseList";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import NavBar from "./NavBar";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const checkUserCredentials = () => {
+    const userType = sessionStorage.getItem("userType");
+    if (userType == 1) {
+      navigate(`/studentPage`);
+    }
+  };
+
+  useEffect(() => {
+    checkUserCredentials();
+  }, []);
+
   return (
     <Box>
+      <CssBaseline />
       <div>
-        <CourseList />
+        <NavBar />
+        <Box>
+          <Typography variant="h4">Course List</Typography>
+          <CourseList />
+        </Box>
       </div>
     </Box>
   );

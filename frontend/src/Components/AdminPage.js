8f7b0bd04dceda29e66973cac6e3e1a716cc3567
Draconis187 <christopherkyledejager@gmail.com>
2026-09-15 23:53:10 +0100

    Changed useEffect for useRef

--- frontend/src/Components/AdminPage.js
+++ frontend/src/Components/AdminPage.js
@@ -1,20 +1,18 @@
 import { Box } from "@mui/material";
 import UserList from "./UserList";
-import { useEffect } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const AdminPage = () => {
   const navigate = useNavigate();
+  const userType = useRef(sessionStorage.getItem("userType"));
 
-  useEffect(() => {
-    const userType = sessionStorage.getItem("userType");
-    if (userType === 1) {
-      navigate(`/studentPage`);
-    } else if (userType === 2) {
-      navigate(`/homePage`);
-    }
-  });
+  if (userType === 1) {
+    navigate(`/studentPage`);
+  } else if (userType === 2) {
+    navigate(`/homePage`);
+  }
 
   return (
     <Box>

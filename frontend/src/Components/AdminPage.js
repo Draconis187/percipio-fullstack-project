12b4f7197754cb25f012714413217ae3bd83f48d
Draconis187 <christopherkyledejager@gmail.com>
2026-09-16 10:45:59 +0100

    Testing use of useRef instead of useEffect

--- frontend/src/Components/AdminPage.js
+++ frontend/src/Components/AdminPage.js
@@ -1,24 +1,18 @@
 import { Box } from "@mui/material";
 import UserList from "./UserList";
-import { useEffect } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const AdminPage = () => {
   const navigate = useNavigate();
+  const userType = useRef(sessionStorage.getItem("userType"));
 
-  const checkUserCredentials = () => {
-    const userType = sessionStorage.getItem("userType");
-    if (userType === 1) {
-      navigate(`/studentPage`);
-    } else if (userType === 2) {
-      navigate(`/homePage`);
-    }
-  };
-
-  useEffect(() => {
-    checkUserCredentials();
-  }, [checkUserCredentials]);
+  if (userType === 1) {
+    navigate(`/studentPage`);
+  } else if (userType === 2) {
+    navigate(`/homePage`);
+  }
 
   return (
     <Box>

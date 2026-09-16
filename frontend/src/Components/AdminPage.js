642ddf22849ff39a81c21a84384e7587d983ea74
Draconis187 <christopherkyledejager@gmail.com>
2026-09-16 10:58:34 +0100

    Undoing changes made, back to the useEffect.

--- frontend/src/Components/AdminPage.js
+++ frontend/src/Components/AdminPage.js
@@ -1,18 +1,24 @@
 import { Box } from "@mui/material";
 import UserList from "./UserList";
-import { useRef } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const AdminPage = () => {
   const navigate = useNavigate();
-  const userType = useRef(sessionStorage.getItem("userType"));
 
-  if (userType === 1) {
-    navigate(`/studentPage`);
-  } else if (userType === 2) {
-    navigate(`/homePage`);
-  }
+  const checkUserCredentials = () => {
+    const userType = sessionStorage.getItem("userType");
+    if (userType === 1) {
+      navigate(`/studentPage`);
+    } else if (userType === 2) {
+      navigate(`/homePage`);
+    }
+  };
+
+  useEffect(() => {
+    checkUserCredentials();
+  }, []);
 
   return (
     <Box>

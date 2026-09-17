0c5959df8982015f0b3c7eebd34f1fcbe80badb9
Draconis187 <christopherkyledejager@gmail.com>
2026-09-17 10:21:10 +0100

    Added additional tests to the login test

--- frontend/src/Tests/login.test.js
+++ frontend/src/Tests/login.test.js
@@ -5,14 +5,25 @@ import "@testing-library/jest-dom";
 const mockedUsedNavigate = jest.fn();
 
 jest.mock("react-router-dom", () => ({
-  // ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockedUsedNavigate,
 }));
 
 describe("Login", () => {
+  test("Heading should display", () => {
+    render(<Login />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
   test("Username input field should exist", () => {
     render(<Login />);
     const username = screen.getByLabelText("Username");
     expect(username).toBeInTheDocument();
+  });
+
+  test("Login button should exist", () => {
+    render(<Login />);
+    const loginButton = screen.getByText("Log in");
+    expect(loginButton).toBeInTheDocument();
   });
 });
